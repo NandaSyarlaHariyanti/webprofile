3144863c03f23085fdca6642ae3b0871bc40c8fe
unknown <nandasyarla58911@gmail.com>
2023-03-08 08:06:41 +0700

    Halaman biografi - Menampilkan biodata

---
+++ biografi.php
@@ -0,0 +1,22 @@
+<div class="halaman">
+
+<?php
+	$nama = "Nanda Syarla Hariyanti";
+	$tempatLahir = "Tulungagung";
+	$tglLahir = '2003-04-09';
+    $agama= "Islam";
+	$alamat = "Jl. Panglima Sudirman, Ngantru, Tulungagung";
+	$pendidikan = " <li> SMPN 2 Tulungagung </li>
+					<li> SMK Telkom Malang </li>";
+
+	echo "<h3>Nama  : ".$nama."<br>";
+	echo "TTL	: " .$tempatLahir."<br>";
+	echo "Tanggal : ".$tglLahir."<br>";
+	echo "Agama : " .$agama."<br>";
+	echo "Alamat : " .$alamat."<br>";
+	echo "Pendidikan :" .$pendidikan."<br> </h3>";
+
+?>
+
+</div>
+    
