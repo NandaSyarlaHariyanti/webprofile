bbdb363fddf4d85959008c80472a620f47a0bbc4
unknown <nandasyarla58911@gmail.com>
2023-03-08 07:55:40 +0700

    Halaman home - Sebagai halaman welcome

---
+++ home.php
@@ -0,0 +1,4 @@
+<div class="halaman">
+    <h1>HALO !</h1>
+    <p>Selamat datang! Ini adalah sebuah web profile yang berisi biodata dan pengalaman saya. Saya berharap, melalui website ini, Anda bisa mengetahui biodata dan pengalaman yang saya miliki!</p>
+</div>
