ebcfd892014099745a377b36c1f744afc94fab83
unknown <nandasyarla58911@gmail.com>
2023-03-08 08:11:36 +0700

    Halaman about - Menampilkan deskripsi saya

---
+++ about.php
@@ -0,0 +1,16 @@
+<div class="halaman">
+	<h2>About me</h2>
+	<p> Saya adalah seorang mahasiswi semester 4 Prodi Informatika,
+		Fakultas Ilmu Komputer di Universitas Pembangunan Nasional "Veteran" Jawa Timur.</p>
+	<p> Selama menempuh pendidikan sampai semester 4 ini, saya mendapatkan
+		banyak sekali pengalaman baru dan juga teman - teman yang berasal dari 
+		berbagai daerah. Meskipun memiliki budaya berbeda, saya merasa kami dapat
+		menghargai budaya masing - masing.
+	</p>
+	<p> Saya suka membaca buku, menonton series, dan pergi ke pantai terutama di sore hari.
+		Bagi saya, pergi ke pantai dengan beberapa teman untuk menikmati sunset sambil berbagi
+		cerita merupakan hal yang sangat istimewa, terutama setelah teman - teman saya memiliki 
+		kesibukan masing - masing. Selain itu, akhir - akhir ini saya suka mencoba berbagai
+		resep baru untuk meningkatkan kemampuan memasak saya.
+	</p>
+</div>
