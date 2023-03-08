5b068b71657a2f3a80a964da2213ec1e35f9084f
unknown <nandasyarla58911@gmail.com>
2023-03-08 08:07:34 +0700

    Halaman biografi - Menampilkan usia

--- biografi.php
+++ biografi.php
@@ -4,6 +4,8 @@
 	$nama = "Nanda Syarla Hariyanti";
 	$tempatLahir = "Tulungagung";
 	$tglLahir = '2003-04-09';
+    $tglSekarang = date('Y-m-d');
+	$usia = date_diff(date_create($tglLahir), date_create($tglSekarang))->y;
     $agama= "Islam";
 	$alamat = "Jl. Panglima Sudirman, Ngantru, Tulungagung";
 	$pendidikan = " <li> SMPN 2 Tulungagung </li>
@@ -12,6 +14,7 @@
 	echo "<h3>Nama  : ".$nama."<br>";
 	echo "TTL	: " .$tempatLahir."<br>";
 	echo "Tanggal : ".$tglLahir."<br>";
+    echo "Usia	: " .$usia." tahun <br>";
 	echo "Agama : " .$agama."<br>";
 	echo "Alamat : " .$alamat."<br>";
 	echo "Pendidikan :" .$pendidikan."<br> </h3>";
