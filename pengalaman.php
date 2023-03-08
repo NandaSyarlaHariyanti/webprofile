dfeb9b43a45dd27577a488184a01f44e91638e16
unknown <nandasyarla58911@gmail.com>
2023-03-08 08:13:13 +0700

    Halaman pengalaman - Menampilkan pengalaman

---
+++ pengalaman.php
@@ -0,0 +1,14 @@
+<div class="halaman">
+    <h3> PEMABA 2022 - Divisi Senior Pendamping </h3>
+        <li>Membantu mahasiswa baru beradaptasi dengan linkungan kampus </li>
+        <li>Membantu mahasiswa baru untuk mengenal sumberdaya kampus</li> <br>
+                            
+    <h3>PEMIRA IF 2023 - Divisi Humas</h3>
+        <li>Mengelola sosial media sebagai media promosi </li>
+        <li>jalin hubungan baik dengan sponsor</li>
+                            
+    <h3> Mesin - Divisi Digital Marketing</h3>
+        <li>Membuat dan mengelola konten digital</li>
+        <li>Menerapkan optimasi SEO </li> <br>
+
+</div>
