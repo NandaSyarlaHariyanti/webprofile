b893deb54a2cac056f3fe7a063d35ad03e189165
unknown <nandasyarla58911@gmail.com>
2023-03-08 07:43:15 +0700

    Halaman index - Tampilan menu pada web

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<div class="container">
+		<div class="profile">
+
+		<table class="tabel">
+                <tr>
+                    <td rowspan="2">
+                        <div class="sosmed">
+                            <img src="profile.png" class="fotodiri">
+							<div class = "grad">
+								<ul>
+										<a href="https://wa.me/+6285604520146" class = "fontsosmed">whatsapp
+										</a>
+
+								</ul>
+							</div>
+							<div class = "grad">
+								<ul>
+										<a href="https://instagram.com/syrla_nh?igshid=NmQ2ZmYxZjA=" class = "fontsosmed">Instagram</a>
+								</ul>
+							</div>
+							<div class = "grad">
+								<ul>
+										<a href="https://www.linkedin.com/in/nanda-syarla-hariyanti-183849266" class = "fontsosmed">Linkedin</a>
+								</ul>
+							</div>
+							<div class = "grad">
+								<ul>
+										<a href="https://github.com/NandaSyarlaHariyanti/" class = "fontsosmed">Github</a>
+								</ul> 
+								</div>
+                        </div>
+                        
+
+                    </td>
+
+                    <td >
+					<div class="bio">
+                            <table class="fontbio">
+					<div class="content">
+                    <div class="menu">
+		<ul>
+			<li><a href="index.php?page=home">HOME</a></li>
+			<li><a href="index.php?page=biografi">BIODATA</a></li>
+			<li><a href="index.php?page=tentang">TENTANG</a></li>
+			<li><a href="index.php?page=pengalaman">Pengalaman</a></li>
+		</ul>
+	</div>
+
+</td >
+
+</body>
